ad053662896c9190718f34ba31ac93def1ca17b6
Christopher Nethercott <ccnethercott@gmail.com>
2018-11-05 20:44:21 +0000

    Added some boilerplate code.

---
+++ commands/leaderboard.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const hypixeljs = require('hypixeljs');
+
+module.exports = {
+	name: 'leaderboard',
+	args: true,
+	description: 'Fetches leaderboards for a given game.',
+	usage: 'leaderboard [game]',
+	execute(message, args) {
+
+	},
+}
