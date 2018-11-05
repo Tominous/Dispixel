615a1c742ad3233d5352d36b2b2a247515926066
Christopher Nethercott <ccnethercott@gmail.com>
2018-11-05 21:31:09 +0000

    Stuff and stuff

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -2,19 +2,29 @@ const Discord = require('discord.js');
 const hypixeljs = require('hypixeljs');
 const fs = require('fs');
 
-const gameConversion = JSON.parse(fs.readFileSync('../lib/gameconversion.json').toString());
+const gameConversion = JSON.parse(fs.readFileSync('./lib/gameconversion.json').toString());
 const allGames = Object.keys(gameConversion);
 
 module.exports = {
 	name: 'leaderboard',
 	args: true,
 	description: 'Fetches leaderboards for a given game.',
-	usage: 'leaderboard [game]',
+	usage: '[game]',
 	execute(message, args) {
 		if (allGames.indexOf(args[0]) > -1) {
 			// makes sure we actually have the game.
 			if (gameConversion[args[0]] != null) {
+				hypixeljs.getLeaderboards((err, leaderboards) => {
+					if (err) console.error(err);
+					const leaderboardEmbed = new Discord.RichEmbed().setTitle(`${args[0]} leaderboard:`).setColor('#7f8fa6');
+					const gameLeaderboard = leaderboards[gameConversion[args[0]]];
 
+					if (gameLeaderboard != null) {
+						gameLeaderboard.forEach(leaderboard => {
+							leaderboardEmbed.addField(`${leaderboard.prefix} ${leaderboard.title}`, `Leaders: ${leaderboard.leaders.join(', ')}`);
+						});
+					}
+				});
 			}
 		} else {
 			message.channel.send(new Discord.RichEmbed()
@@ -24,4 +34,4 @@ module.exports = {
 			);
 		}
 	},
-}
+};
